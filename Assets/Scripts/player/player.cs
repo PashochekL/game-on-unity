1fb68428b0df6ee7abfcca5f374a94ef31cb5d51
Linberg34 <pfqxbr2208@gmail.com>
2024-06-14 20:54:39 +0700

    added person, some anims, few visualization bugs

--- Assets/Scripts/player/player.cs
+++ Assets/Scripts/player/player.cs
@@ -14,16 +14,23 @@ public class player : MonoBehaviour
     private float _jumpYVel;
     private Vector2 _modeVel;
     private bool _isOnSurface;
+    private bool _isDead;
+
+  
+
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
-
 
     private void Update()
     {
         GetInput();
+        UpdateAnimations();
+        Suicide();
     }
 
     private void FixedUpdate()
@@ -42,6 +49,8 @@ public class player : MonoBehaviour
             _modeVel = _rigidBody.velocity;
             _modeVel.y = _jumpYVel;
             _rigidBody.velocity = _modeVel;
+
+            _animator.SetBool("isJumping", true);
         }
     }
 
@@ -50,24 +59,43 @@ public class player : MonoBehaviour
         _modeVel = _rigidBody.velocity;
         _modeVel.x = _moveDir * moveSpeed * Time.fixedDeltaTime;
         _rigidBody.velocity = _modeVel;
+
+        _animator.SetBool("isRunning", MathF.Abs(_moveDir) > 0);
     }
 
     private float CalculateJumpVel(float height)
-    {/*
-        return MathF.Sqrt((-2 * _rigidBody.gravityScale * Physics2D.gravity.y * height));*/
+    {
         return MathF.Sqrt((-2 * _rigidBody.gravityScale * Physics2D.gravity.y * height));
+    }
+
+    private void Suicide()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _isDead = true;
+            _animator.SetBool("isDead", true);
+
+        }
     }
 
     void GetInput()
     {
         _moveDir = Input.GetAxis("Horizontal");
         _jumpPressed |= Input.GetKeyDown(KeyCode.Space);
+        
+    }
 
+    private void UpdateAnimations()
+    {
+        _animator.SetBool("isDead", _isDead);
+        _animator.SetBool("isOnSurface", _isOnSurface);
+        
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         _isOnSurface = true;
+        _animator.SetBool("isJumping", false); 
     }
 
     private void OnCollisionExit2D(Collision2D other)
